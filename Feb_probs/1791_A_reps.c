2d71c77f4150ba57cbfae15c683c0fbd8128d355
Graham <parrgraham888@gmail.com>
2023-02-16 15:48:43 +1300

    func:added proper code
    this is a good solution ,I have done some reps too

---
+++ Feb_probs/1791_A_reps.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<string.h>//string.h    string.h    ctype.h ctype.h ctype.h string.h
+/*
+int t;  scanf("%d",&t); int t;  scanf("%d",&t);     int t;  scanf("%d",&t)  "%d",&t "%d",&t
+char c; scanf("%c",&c)  ("%c",&c)   ("%c",&c)   ("%c".&c)   ()
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(){
+    int t;
+    int i;
+    char c;
+
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+        scanf("%c",&c);
+        char* s = "codeforces";
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- Feb_probs/1791_A_solve.c
+++ Feb_probs/1791_A_solve.c
@@ -1,5 +1,36 @@
-#include<stdio.h>
-int main(){
-    printf("hello world");
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    int t, i;
+    char c;
+
+    // read the number of test cases
+    scanf("%d", &t);
+
+    // process each test case
+    for(i = 0; i < t; i++) {
+        // read the input character
+        scanf(" %c", &c);
+
+        // check if the character appears in the string "codeforces"
+        char* s = "codeforces";
+        int len = strlen(s);
+        int j, count = 0;
+        for(j = 0; j < len; j++) {
+            if(tolower(c) == s[j]) {
+                count++;
+            }
+        }
+
+        // output the result
+        if(count > 0) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
     return 0;
 }
